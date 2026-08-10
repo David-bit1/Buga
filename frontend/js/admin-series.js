637c33059125408ff5085eeb17afb9a8e718c821
David <david@kali.Guadalupe>
2026-08-10 14:41:55 -0600

    corigiendo bugs

---
+++ frontend/js/admin-series.js
@@ -0,0 +1,12 @@
+(function () {
+const ADMIN_API = '/api/admin';
+
+const seriesForm = document.getElementById('seriesForm');
+const seriesId = document.getElementById('seriesId');
+const seriesTmdbId = document.getElementById('seriesTmdbId');
+const seriesTitle = document.getElementById('seriesTitle');
+const seriesOriginalTitle = document.getElementById('seriesOriginalTitle');
+const seriesOverview = document.getElementById('seriesOverview');
+const seriesDescription = document.getElementById('seriesDescription');
+const seriesPosterUrl = document.getElementById('seriesPosterUrl');
+const seriesBannerUrl = document.getElement<|tool_call_begin|>api<|tool_call_begin|><|tool_call_begin|>orm<|tool_call_begin|>ant<|tool_call_begin|>a
